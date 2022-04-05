728eccc6d4e0a416e44d93584cf93e75bc289cd4
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-05 14:03:37 -0300

    feat: primeiras query typeDefs e resolvers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-const blah = "foo";
+import { ApolloServer, gql } from "apollo-server";
 
-console.log(blah);
+const typeDefs = gql`
+  type Query {
+    hello: String
+    name: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Olá Mundo!";
+    },
+    name: () => {
+      return "Daniel Silva";
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen(4004).then(({ url }) => {
+  console.log(url);
+});
