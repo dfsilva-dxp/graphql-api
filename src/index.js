f8ccf51e300a99a27aeadaceec7c09382a21a007
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-05 17:07:48 -0300

    feat: query Custom Type list

--- src/index.js
+++ src/index.js
@@ -11,9 +11,17 @@ const typeDefs = gql`
     date: String
   }
 
+  type Product {
+    id: ID!
+    name: String!
+    price: Float!
+    quantity: Int!
+  }
+
   type Query {
     currentDate: String!
     customer: Customer!
+    products: [Product!]
   }
 `;
 
@@ -48,6 +56,35 @@ const resolvers = {
         married: true,
         currentDate: new Date()
       };
+    },
+
+    products: () => {
+      return [
+        {
+          id: "uid-002",
+          name: "Orange",
+          price: 12.5,
+          quantity: 260
+        },
+        {
+          id: "uid-003",
+          name: "Apple",
+          price: 4.99,
+          quantity: 377
+        },
+        {
+          id: "uid-004",
+          name: "Strawbery",
+          price: 8.99,
+          quantity: 725
+        },
+        {
+          id: "uid-005",
+          name: "Banana",
+          price: 5.8,
+          quantity: 1022
+        }
+      ];
     }
   }
 };
