6a88234f9732b5dfe97cec32a6c2245f838f0371
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-05 16:42:52 -0300

    feat: resolver customer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Customer {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int!
+    weight: Float!
+    married: Boolean!
+    date: String
+  }
+
   type Query {
     currentDate: String!
+    customer: Customer!
   }
 `;
 
 const resolvers = {
+  Customer: {
+    date: (customer) => {
+      return new Intl.DateTimeFormat("pt-BR", {
+        day: "numeric",
+        month: "numeric",
+        year: "numeric"
+      }).format(customer.currentDate);
+    }
+  },
   Query: {
     currentDate: () => {
       const date = new Date();
@@ -16,6 +36,18 @@ const resolvers = {
         month: "numeric",
         year: "numeric"
       }).format(date);
+    },
+
+    customer: () => {
+      return {
+        id: "uid-001",
+        name: "Daniel Silva",
+        email: "dfsilva.dxp@gmail.com",
+        age: 34,
+        weight: 88.9,
+        married: true,
+        currentDate: new Date()
+      };
     }
   }
 };
