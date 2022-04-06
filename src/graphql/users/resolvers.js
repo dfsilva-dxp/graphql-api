3d9aa53d6c856467cb055eba0c1b13d834a2b14c
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-06 16:55:51 -0300

    feat: refatorando código

--- src/graphql/users/resolvers.js
+++ src/graphql/users/resolvers.js
@@ -1,14 +1,12 @@
 export const userResolvers = {
   Query: {
     user: async (_, { id }, { api }) => {
-      const data = await api
-        .get(`users/${id}`)
-        .then((response) => response.data);
+      const data = await api.get(`users/${id}`).then(({ data }) => data);
 
       return data;
     },
     users: async (_, __, { api }) => {
-      const data = await api.get("users").then((response) => response.data);
+      const data = await api.get("users").then(({ data }) => data);
 
       return data;
     }
