b2bd6719c2ea2fb66d9d7f52c5d2e45e417434bf
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-06 15:33:03 -0300

    feat: criando o contexto

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import { ApolloServer } from "apollo-server";
 
+import { api } from "./services/api";
+
 import { typeDefs, resolvers } from "./graphql/schema";
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+
+  context: () => {
+    return {
+      api
+    };
+  }
 });
 
 server.listen(4004).then(({ url }) => {
